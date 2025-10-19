54a5c4c7fbe145c252a548629a575af9bea53431
Daniel Lehre Ekeberg <danieleke@live.no>
2025-10-19 22:30:42 +0200

    Update Section h-size

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,10 +1,10 @@
 function Section() {
     return (
         <section className="h-[73vh] flex flex-col justify-end">
-            <h1 className="text-9xl font-bold">Front-end Developer</h1>
+            <h1 className="text-8xl font-bold">Front-end Developer</h1>
             <div className="flex gap-7">
-                <h1 className="text-9xl font-bold text-neutral-700">and</h1>
-                <h1 className="text-9xl font-bold">Web Designer</h1>
+                <h1 className="text-8xl font-bold text-neutral-700">and</h1>
+                <h1 className="text-8xl font-bold">Web Designer</h1>
             </div>
             
         </section>
