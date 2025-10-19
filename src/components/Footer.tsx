5586678e4d7436a0e5f17df7b07468690a1b0d77
Daniel Lehre Ekeberg <danieleke@live.no>
2025-10-19 23:36:19 +0200

    No longer hardcoded year in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
 function Footer() {
+    const time = new Date();
+    const year = time.getFullYear();
+    
     return (
         <div className="flex justify-between mt-20 py-10 text-neutral-600">
-            <p>Ekeberg, 2025</p>
-            <div className="flex gap-10">
+            <p>Ekeberg, {year}</p>
+            <div className="flex gap-5">
                 <a href="https://github.com/danielekeberg" target="_blank">
                     <img src="github.svg" alt="GitHub" className="w-6 h-6" />
                 </a>
