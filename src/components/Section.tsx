eb9cc0a55c6c721c32de47a1e328c9125cfe14dc
Daniel Lehre Ekeberg <danieleke@live.no>
2025-10-20 15:38:41 +0200

    Shorten tailwindcss for section size

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,13 +1,13 @@
 function Section() {
     return (
-        <section className="h-[73vh] flex flex-col justify-end">
+        <section className="h-[73vh] flex flex-col justify-end text-8xl">
             <div className="flex gap-7">
-                <h1 className="text-8xl font-bold">Front-end</h1>
-                <h1 className="text-8xl font-bold text-neutral-700">Developer</h1>
+                <h1 className="font-bold">Front-end</h1>
+                <h1 className="font-bold text-neutral-700">Developer</h1>
             </div>
             <div className="flex gap-7">
-                <h1 className="text-8xl font-bold text-neutral-700">and</h1>
-                <h1 className="text-8xl font-bold">Web Designer</h1>
+                <h1 className="font-bold text-neutral-700">and</h1>
+                <h1 className="font-bold">Web Designer</h1>
             </div>
             
         </section>
