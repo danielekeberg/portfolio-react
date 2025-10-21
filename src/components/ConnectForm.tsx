5aaf52ad32ac3bd713c529ce1879582acca61f4b
Daniel Lehre Ekeberg <danieleke@live.no>
2025-10-21 18:06:29 +0200

    Create functional contact form

---
+++ src/components/ConnectForm.tsx
@@ -0,0 +1,82 @@
+'use client';
+import { useState, FormEvent } from "react";
+
+function ConnectForm() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [status, setStatus] = useState("");
+
+    async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        setStatus("Sending...");
+        const now = new Date();
+        const hours = now.getHours().toString().padStart(2, "0");
+        const minutes = now.getMinutes().toString().padStart(2, "0");
+        const day = now.getDate().toString().padStart(2, "0");
+        const month = (now.getMonth() + 1).toString().padStart(2, "0");
+        const year = now.getFullYear();
+
+        const formatted = `${hours}:${minutes} - ${day}.${month}.${year}`
+        try {
+            const res = await fetch("https://api.sheetbest.com/sheets/a76cd739-4604-4c7e-856c-f1506d6b8708", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    date: formatted,
+                    firstname: firstName.trim(),
+                    lastname: lastName.trim(),
+                    email: email.trim(),
+                    message: message.trim(),
+                }),
+            });
+
+            if(!res.ok) {
+                const err = await res.text();
+                throw new Error((err || "Couldnt send message"));
+            }
+            setStatus("Message sent!")
+            setTimeout(() => {
+                setStatus("");
+            }, 2000)
+        } catch(err) {
+            setStatus(`Error: ${err}`)
+        }
+    }
+    return (
+        <form onSubmit={handleSubmit} className="flex justify-center relative">
+            <div>
+                <div className="flex gap-5 mb-5">
+                    <div className="relative">
+                        <input className="peer border border-neutral-800 rounded pt-5 pb-1 px-2" placeholder=" " type="text" id="fname" onChange={(e) => setFirstName(e.target.value)} />
+                        <label htmlFor="fname" className="absolute left-2 text-neutral-400 transition-all peer-placeholder-shown:top-3.5 peer-placeholder-shown:text-neutral-300 peer-placeholder-shown:text-base peer-focus:top-1 peer-focus:text-xs">First Name</label>
+                    </div>
+                    <div className="relative">
+                        <input className="peer border border-neutral-800 rounded pt-5 pb-1 px-2" placeholder=" " type="text" id="lname" onChange={(e) => setLastName(e.target.value)} />
+                        <label htmlFor="fname" className="absolute left-2 text-neutral-400 transition-all peer-placeholder-shown:top-3.5 peer-placeholder-shown:text-neutral-300 peer-placeholder-shown:text-base peer-focus:top-1 peer-focus:text-xs">Last Name</label>
+                    </div>
+                </div>
+                <div className="flex gap-5 mb-5">
+                    <div className="relative">
+                        <input className="peer border border-neutral-800 rounded pt-5 pb-1 px-2" placeholder=" " type="text" id="email" onChange={(e) => setEmail(e.target.value)} />
+                        <label htmlFor="email" className="absolute left-2 text-neutral-400 transition-all peer-placeholder-shown:top-3.5 peer-placeholder-shown:text-neutral-300 peer-placeholder-shown:text-base peer-focus:top-1 peer-focus:text-xs">Email</label>
+                    </div>
+                </div>
+                <div className="flex gap-5 mb-5">
+                    <textarea className="border border-neutral-800 rounded resize-none h-50 p-2 w-full" placeholder="Message" id="msg" onChange={(e) => setMessage(e.target.value)} />
+                </div>
+                <div>
+                    <button type="submit" className="cursor-pointer border border-neutral-800 rounded-md hover:bg-neutral-900 w-full p-2 active:bg-neutral-600">SUBMIT</button>
+                </div>
+                {status && (
+                    <div className="p-3 fixed right-10 top-25 italic border border-neutral-850 bg-neutral-900 rounded z-1000 cursor-pointer ">
+                        <p>{status}</p>
+                    </div>
+                )}
+            </div>
+        </form>
+    )
+}
+
+export default ConnectForm;
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,13 +1,18 @@
+import ConnectForm from "@/components/ConnectForm";
+
 function Contact() {
     return (
         <div id="contact" className="h-[80vh] py-20">
-            <div className="flex gap-6">
+            <div className="flex gap-6 mb-20">
                 <h1 className="text-6xl font-bold">Let's</h1>
                 <h1 className="text-6xl font-bold text-neutral-600">Connect</h1>
+                <div className="h-auto w-full mb-1 rounded flex items-center opacity-20">
+                    <div className="h-0.5 bg-white w-full"></div>
+                </div>
             </div>
-            <div className="flex h-full flex-col justify-end">
-                <h4 className="text-3xl text-neutral-500">Feel free to contact me if you have any questions or inquiries.</h4>
-            </div>
+            
+            <ConnectForm />
+            <h4 className="h-[20vh] flex flex-col justify-end text-2xl text-neutral-500">Feel free to contact me if you have any questions or inquiries.</h4>
         </div>
     )
 }
